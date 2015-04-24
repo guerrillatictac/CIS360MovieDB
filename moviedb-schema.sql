e101f6879f6ef861a5c6b8679f417ef598c5776f
guerrillatictac <guerrillatictac@users.noreply.github.com>
2015-04-24 00:43:03 -0400

    Updated movie db schema without fk constraints
    This version does not have constraints and will run to create tables.

--- moviedb-schema.sql
+++ moviedb-schema.sql
@@ -1,24 +1,9 @@
---DROP TABLE employee CASCADE CONSTRAINTS;
---DROP TABLE department CASCADE CONSTRAINTS;
---DROP TABLE dept_locations CASCADE CONSTRAINTS;
---DROP TABLE project CASCADE CONSTRAINTS;
---DROP TABLE works_on CASCADE CONSTRAINTS;
---Taylor Wilcox, Kyle Williams, Jordan Sumner;
-CREATE TABLE Actors (
-  fname    varchar2(15) not null, 
-  lname    varchar2(15) not null,
-  BaconNo      char(7),
-  Networth    number(9),
-  Awards  varchar2(42),
-  primary key (BaconNo),
-  foreign key (Awards) references Awards(award)
-  
-);
 
-CREATE TABLE Movies (
-  Title        varchar2(25) not null,
-  Director      number(4),
-  YearReleased char(9) not null, 
+--Taylor Wilcox, Kyle Williams, Jordan Sumner;
+CREATE TABLE Movies(
+  Title varchar2(42) not null,
+  Director number(4) not null,
+  YearReleased number(4) not null, 
   Runtime number(4),
   Rating varchar2(5),
   Budget number(10),
@@ -26,127 +11,110 @@ CREATE TABLE Movies (
   Writer varchar(25),
   Awards varchar2(100),
   
-  primary key (Title,Director,YearReleased)
+  primary key (Title)
+  
+);
+
+CREATE TABLE MovieAwards(
+  Title   varchar2(42) not null,
+  maAwardName    varchar2(42),
+  Yearawarded  number(4),
+  primary key (maAwardName)
+
+);
+
+CREATE TABLE Actors (
+  BaconNo      number(6) not null,
+  fname    varchar2(25) not null, 
+  lname    varchar2(25) not null,
+  Networth    number(9),
+  Awards  varchar2(42),
+  primary key (BaconNo, fname, lname)
 
 );
 
 CREATE TABLE FilmStudio (
-  StudioID  char(25) not null,
-  Name      number(4),
-  CEO       varchar2(15) not null, 
-  YearFounded date,
-  Location varchar(25),
-  primary key (StudioID,Location)
-
-);
-
-CREATE TABLE ProductionCompany (
-  CompanyID        varchar2(25) not null,
-  Name      number(4),
-  CEO       char(9) not null, 
-  YearFounded date,
-  Headquarters char(),
-  primary key (CompanyID,Headquarters)
-
+  StudioID  number(9) not null,
+  stName      varchar2(25) not null,
+  CEO       varchar2(25) not null, 
+  YearFounded NUMBER(4),
+  sLocation varchar2(25),
+  primary key (StudioID)
 );
 
 
 CREATE TABLE FilmStudioLocation (
-  StudioID   number(4),
-  sLocation varchar2(15), 
-  primary key (StudioID,sLocation),
-  foreign key (StudioID) references FilmStudio(StudioID),
-  foreign key (Location) references FilmStudio(Locationof)
+  StudioID   number(9),
+  sLocation varchar2(15)
+  
+);
 
+CREATE TABLE ProductionCompany (
+  CompanyID number(9) not null,
+  Name      varchar2(25) not null,
+  CEO       varchar2(25) not null, 
+  YearFounded number(4),
+  Headquarters varchar2(25),
+  primary key (CompanyID)
+  
+);
+
+CREATE TABLE Produces (
+  CompanyID number(9),
+  Title varchar2(25)
+
+);
+
+CREATE TABLE Pays (
+  CompanyID number(9),
+  StudioID number(9)  
 );
 
 CREATE TABLE ActorAwards (
-  Fname   number(4),
-  Lname varchar2(15), 
-  AwardName,
-  YearAwarded,
-  primary key (Fname, Lname, AwardName, YearAwarded),
-  foreign key (Fname) references Actor(Fname),
-  foreign key (Lname) references Actor(Lname)
-
+  Fname   varchar2(25) not null,
+  Lname varchar2(25) not null, 
+  AwardName varchar2(42),
+  YearAwarded number(4),
+  primary key (Fname, Lname, AwardName, YearAwarded)
 );
 
 CREATE TABLE ActsIn (
-  Fname   number(4),
-  Lname varchar2(15), 
-  Title,
-  Yearacted,
-  primary key (Fname, Lname, Title, Yearacted),
-  foreign key (Fname) references Actor(Fname),
-  foreign key (Lname) references Actor(Lname)
+  Fname   varchar2(25),
+  Lname varchar2(25), 
+  Title varchar2(25),
+  Yearacted integer,
+  primary key (Fname, Lname, Title, Yearacted)
 
 );
 
 CREATE TABLE Creates (
-  StudioID   number(4),
-  YrReleased varchar2(15), 
-  Title,
-  primary key (Title, YrReleased, Title),
-  foreign key (StudioID) references FilmStudio(StudioID),
-  foreign key (Title) references Movie(Title)
+  StudioID   number(9),
+  YrReleased number(4), 
+  Title varchar2(25),
+  primary key (Title, YrReleased)
 
 );
 
-CREATE TABLE project (
-  pname      varchar2(25) not null,
-  pnumber    number(4),
-  plocation  varchar2(15),
-  dnum       number(4) not null,
-  primary key (pnumber),
-  unique (pname),
-  foreign key (dnum) references department(dnumber)
-
-);
-
-CREATE TABLE MovieAwards (
-  Title   char(9),
-  AwardName    number(4),
-  Yearawarded  number(4,1),
-  primary key (Title,AwardName,Yearawarded),
-  foreign key (Title) references Movies(Title),
-
-);
 
 CREATE TABLE WorksFor (
-  Fname   number(4),
-  Lname varchar2(15), 
-  StudioID,
-  primary key (Fname, Lname, StudioID),
-  foreign key (Fname) references Actor(Fname),
-  foreign key (Lname) references Actor(Lname),
-  foreign key (StudioID) references FilmStudio(StudioID)
+  Fname  varchar2(25) not null,
+  Lname varchar2(25) not null, 
+  StudioID number(9),
+  primary key (Fname, Lname, StudioID)
 
 );
 
 CREATE TABLE MovieWriters (
-  Fname   number(4),
-  Lname varchar2(15), 
-  Title,
-  Yr,
-  primary key (Fname, Lname, Title, Yr),
-  foreign key (Title) references Movies(Title)
+  Fname varchar2(25),
+  Lname varchar2(25), 
+  Title varchar2(25),
+  Yr number(4),
+  primary key (Fname, Lname, Title, Yr)
+
 
 );
 
-CREATE TABLE Produces (
-  CompanyID   number(4),
-  Title varchar2(15), 
-  primary key (CompanyID, Title),
-  foreign key (CompanyID) references Company(CompanyID),
-  foreign key (Title) references Movies(Title)
 
-);  
 
-CREATE TABLE Pays (
-  CompanyID   number(4),
-  StudioID varchar2(15), 
-  primary key (CompanyID, StudioID),
-  foreign key (CompanyID) references Company(CompanyID),
-  foreign key (StudioID) references FilmStudio(StudioID)
 
-);
+
