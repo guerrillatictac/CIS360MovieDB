ea6ddaafa3c137d1656b3c5157f3a819359f92b9
Taylor Wilcox <guerrillatictac@users.noreply.github.com>
2015-04-26 22:47:49 -0400

    Update and rename Updated Insert statements to Updated Insert statements.sql

---
+++ Updated Insert statements.sql
@@ -0,0 +1,78 @@
+--Taylor W. edit 4/26/15 to Jordan and Kyles Code. 
+INSERT INTO Actors VALUES 
+  (200001,'Sam','Worthington',12000000,null);
+INSERT INTO Actors VALUES 
+  (200002,'Billy','Zane',20000000,null);
+INSERT INTO Actors VALUES 
+  (200003,'Leonardo','DiCaprio',220000000,null);
+INSERT INTO Actors VALUES
+  (100001,'Mark','Ruffalo',20000000,null);
+INSERT INTO Actors VALUES
+  (200004,'Robert','Downey Jr',170000000,null);
+INSERT INTO Actors VALUES
+  (200005,'Emma','Watson',60000000,null);
+INSERT INTO Actors VALUES
+  (200006,'Daniel','Radcliffe',110000000,null);
+INSERT INTO Actors VALUES
+  (200007,'Idina','Menzel',10000000,null);
+INSERT INTO Actors VALUES
+  (200008,'Don','Cheadle',35000000,null);
+INSERT INTO Actors VALUES
+  (200009,'Vin','Diesel',75000000,null);
+INSERT INTO Actors VALUES
+  (200010,'Paul','Walker',25000000,null);
+INSERT INTO Actors VALUES
+  (200011,'Shia','LaBeouf',25000000,null);
+INSERT INTO Actors VALUES
+  (100002,'Elijah','Wood',30000000,null);
+INSERT INTO Actors VALUES
+  (200012,'Daniel','Craig',65000000,null);
+INSERT INTO Actors VALUES
+  (200013,'Christian','Bale',80000000,null);
+INSERT INTO Actors VALUES
+  (200014,'Clifton','Webb',null,null);
+
+
+
+INSERT INTO Movies VALUES 
+  ('Avatar','James Cameron',2009,161,'pg-13',237,2787,'James Cameron',null);
+INSERT INTO Movies VALUES
+  ('Titanic','James Cameron',1997,194,'R',200,2186,'James Cameron',null);
+INSERT INTO Movies VALUES
+  ('The Avengers','Joss Whedon',2012,143,'pg-13',220,1518,'Joss Whedon',null);
+INSERT INTO Movies VALUES
+  ('Harry Potter and the Deathly Hallows - Part 2','David Yates',2011,130,'pg-13',250,1341,'Steve Kloves',null);
+INSERT INTO Movies VALUES
+  ('Frozen','Chris Buck',2013,102,'pg-13',150,1279,'Jennifer Lee',null);
+INSERT INTO Movies VALUES
+  ('Iron Man 3','Shane Black',2013,130,'pg-13',200,1215,'Drew Pearce',null);
+INSERT INTO Movies VALUES
+  ('Furious 7','James Wan',2015,137,'pg-13',190,1152,'Chris Morgan',null);
+INSERT INTO Movies VALUES
+  ('Transformers: Dark of the Moon','Michael Bay',2011,154,'pg-13',195,1123,'Ehren Kruger',null);
+INSERT INTO Movies VALUES
+  ('The Lord of the Rings: Return of the King', 'Peter Jackson', 2003, 165, 'pg-13', 94, 1119, 'Fran Walsh', null);
+INSERT INTO Movies VALUES
+  ('Skyfall', 'Sam Mendes', 2012, 143, 'pg-13', 200, 1108, 'Neal Purvis', null);
+INSERT INTO Movies VALUES
+  ('The Dark Knight Rises', 'Christopher Nolan', 2012, 165, 'pg-13', 230, 1084, 'Johnathan Nolan', null);
+INSERT INTO Movies VALUES
+  ('Titanic', 'JeanNegulesco', 1953, 98, 'pg', 1,4, 'Charles Bracket', null);
+
+INSERT INTO filmstudio VALUES
+  (100001,'Twentieth Century Fox Film Corporation','Jim Gianopulos',1935,null);
+INSERT INTO filmstudio VALUES
+  (100002,'Marvel Studios','Kevin Feige',1993,null);
+INSERT INTO filmstudio VALUES
+  (100003,'Warner Bros','Kevin Tsujihara',1923,null);
+INSERT INTO filmstudio VALUES
+  (100004,'Walt Disney Animation Studios','Edwin Catmull',1923,null);
+INSERT INTO fimlstudio VALUES
+  (100005,'Universal Pictures','Ronald Meyer',1912,null);
+INSERT INTO filmstudio VALUES
+  (100006,'Paramount Pictures','Brad Grey', 1912,null);
+INSERT INTO filmstudio VALUES
+  (100007,'New Line Cinema','Toby Emmerich',1967,null);
+INSERT INTO filmstudio VALUES
+  (100008,'Eon Productions','Michael Wilson',1961,null);
+  
